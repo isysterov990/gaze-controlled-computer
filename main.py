15d140ab489ca3e506a003f582a644864d6510b4
isysterov990 <62118155+isysterov990@users.noreply.github.com>
2023-02-02 15:52:00 -0500

    Do nothing unless face is detected

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-import numpy as np
-import cv2
-import blink_detector
-from face_detector import *
+from detectors.face_detector import *
+from detectors.blink_detector import *
 
 capture = cv2.VideoCapture(0)
 blink_count=0
 while(1):
     _, input = capture.read() #used _ to ignore the boolean returned by .read()
-    blink_count += blink_detector.ear_detector(input)
+    grayscale = cv2.cvtColor(input, cv2.COLOR_BGR2GRAY)
+    rects = detector(grayscale, 0)
+    if len(rects) > 0:
+        rect = rects[0]
+    else:
+        cv2.imshow("Frame", input)
+        key = cv2.waitKey(1) & 0xFF
+        continue
+    blink_count += ear_detector(input,grayscale,rect)
     print("blink_count", blink_count)
     eye_frame = feature_detector(input)
 
