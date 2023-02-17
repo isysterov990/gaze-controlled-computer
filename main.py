9a457a780fe2fb54d48473ecfc4737319265e899
Ken <kenguo2231@hotmail.com>
2023-02-16 22:40:36 -0500

    combined keypoint averaging and scaling. also added cursor movement.

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ from detectors.feature_detector import *
 from detectors.blink_detector import *
 from detectors.keypoint_detector import *
 from mouse_control import *
+import pyautogui
 
 capture = cv2.VideoCapture(0)
 blink_count=0
+
+average_x = []
+average_y = []
+count = 0
+
+
 while(1):
     _, input = capture.read() #used _ to ignore the boolean returned by .read()
+    input = cv2.flip(input, 1)
     #grayscale = cv2.cvtColor(input, cv2.COLOR_BGR2GRAY)
     # rects = detector(grayscale, 0)
     # if len(rects) > 0:
@@ -22,7 +30,25 @@ while(1):
     if eye_frames is not None:
         keypoints = find_keypoints(eye_frames)
         if keypoints:
-            print('keypoint: ', keypoints[0].pt[0], keypoints[0].pt[1])
+            x = keypoints[0].pt[0]
+            y = keypoints[0].pt[1]
+            if count == 20:
+                move_mouse(average_x, average_y)
+                #print('keypoint: ', keypoints[0].pt[0], keypoints[0].pt[1])
+
+                # print('keypoint: ', x, y)
+                #pyautogui.moveTo(x, y, 1) 
+
+            else:
+                average_x.append(x)
+                average_y.append(y)
+
+  
+    #print('count: ', count)
+    if count > 20:
+        count = 0
+    else:
+        count += 1
 
     k = cv2.waitKey(30) & 0xff
     if k == 27:
--- mouse_control.py
+++ mouse_control.py
@@ -1,23 +1,67 @@
 import pyautogui
 
-def move_mouse(x, y):
-    # implement mouse movement
-    pyautogui.moveTo(x, y, 1) 
-        # commented out for now as this is not effecient and leads to delays
-    # if keypoints:
-    #     x, y = keypoints[0].pt
-    #     pyautogui.FAILSAFE=False
-    #     # determine the direction by comparing the x, y coordinates of the keypoints
-    #     if x > w_1 / 2:
-    #         print("Eyes are facing right")
-    #         pyautogui.moveRel(-25, 0)
-    #     elif x < w_1 / 2:
-    #         print("Eyes are facing left")
-    #         pyautogui.moveRel(25, 0)
-    #     if y > h_1 / 2:
-    #         print("Eyes are facing down")
-    #         pyautogui.moveRel(0, 25)
-    #     elif y < h_1 / 2:
-    #         print("Eyes are facing up")
-    #         pyautogui.moveRel(0, -25)
+current_x, current_y = pyautogui.position()
+
+kp1_x_min = 100000
+kp1_y_min = 100000
+kp1_x_max = 0
+kp1_y_max = 0
+screen_x = 1920
+screen_y = 1080
+def move_mouse(average_x, average_y):
+    pyautogui.FAILSAFE = False
+    global kp1_x_min 
+    global kp1_y_min 
+    global kp1_x_max
+    global kp1_y_max
+
+    if len(average_x) > 0 and len(average_y) > 0:
+
+        avg_x = 0
+        for av in average_x:
+            avg_x += av
+        avg_x /= len(average_x)
+        avg_y = 0
+        for av in average_y:
+            avg_y += av
+        avg_y /= len(average_y)
+        # print('avg_x: ', avg_x)
+        # print('avg_y: ', avg_y)
+
+        # Updating min/max boundaries
+        if avg_x < kp1_x_min:
+            kp1_x_min = avg_x
+        elif avg_x > kp1_x_max:
+            kp1_x_max = avg_x 
+
+        if avg_y < kp1_y_min:
+            kp1_y_min = avg_y
+        elif avg_y > kp1_y_max:
+            kp1_y_max = avg_y
+
+        # print('x_min: ', kp1_x_min)
+        # print('x_max: ', kp1_x_max)
+        # print('y_min: ', kp1_y_min)
+        # print('y_max: ', kp1_y_max)
+
+        scaled_x = (((avg_x - kp1_x_min)/(kp1_x_max - kp1_x_min)) * (screen_x))
+        scaled_y = (((avg_y - kp1_y_min)/(kp1_y_max - kp1_y_min)) * (screen_y))
+
+        # print('scaled_x: ', scaled_x)
+        # print('scaled_y: ', scaled_y)
+        pyautogui.moveTo(scaled_x, scaled_y, 0.5) 
+        # if avg_x > w_1 / 2:
+        #     print(f"Eyes are facing right {x} {y}")
+        #     pyautogui.moveRel(-30, 0)
+        # elif avg_x < w_1 / 2:
+        #     print(f"Eyes are facing left {x} {y}")
+        #     pyautogui.moveRel(30, 0)
+        # if avg_y > h_1 / 1.9:
+        #     print(f"Eyes are facing down {x} {y}")
+        #     pyautogui.moveRel(0, 30)
+        # elif avg_y < h_1 / 1.7:
+        #     print(f"Eyes are facing up {x} {y}")
+        #     pyautogui.moveRel(0, -30)
+        average_x.clear()
+        average_y.clear()
     
