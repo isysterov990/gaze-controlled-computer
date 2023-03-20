c1652c7a970b3046be10eaba8592d1f2ee164067
JakeApted <mr.apted@gmail.com>
2023-03-19 22:54:55 -0400

    combining all commits from blinking, mouse movements and keypoints averaging

--- detectors/blink_detector.py
+++ detectors/blink_detector.py
@@ -1,26 +1,33 @@
-# import cv2
-# from imutils import face_utils
-# from utils import math_utils
-# import dlib
+import cv2
+from imutils import face_utils
+from utils import math_utils
+from mouse_control import *
 
-# detector = dlib.get_frontal_face_detector()
-# predictor = dlib.shape_predictor("./models/shape_predictor_68_face_landmarks.dat")
-# EYE_BLINK_CONSTANT = 0.25
-# (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-# (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+import dlib
 
-# def ear_detector(input, grayscale ,rect):
-#     blink_count=0
-#     landmarks = predictor(grayscale, rect)
-#     landmarks = face_utils.shape_to_np(landmarks)
-#     leftEyeLandmarks = landmarks[lStart:lEnd]
-#     rightEyeLandmarks = landmarks[rStart:rEnd]
-#     leftEAR = math_utils.compute_ear(leftEyeLandmarks)
-#     rightEAR = math_utils.compute_ear(rightEyeLandmarks)
-#     ear = (leftEAR + rightEAR) / 2.0
-#     if ear < EYE_BLINK_CONSTANT:
-#         cv2.putText(input, "Blink", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-#         blink_count+=1
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("./models/shape_predictor_68_face_landmarks.dat")
+EYE_BLINK_CONSTANT = 0.25
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+def ear_detector(input):
+    blink_count=0
+    grayscale = cv2.cvtColor(input, cv2.COLOR_BGR2GRAY)
+    rects = detector(grayscale, 0)
+    rect = rects[0]
+    landmarks = predictor(grayscale, rect)
+    landmarks = face_utils.shape_to_np(landmarks)
+    leftEyeLandmarks = landmarks[lStart:lEnd]
+    rightEyeLandmarks = landmarks[rStart:rEnd]
+    leftEAR = math_utils.compute_ear(leftEyeLandmarks)
+    rightEAR = math_utils.compute_ear(rightEyeLandmarks)
+    ear = (leftEAR + rightEAR) / 2.0
+    if ear < EYE_BLINK_CONSTANT:
+        cv2.putText(input, "Blink", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+        blink_count+=1
+        print("click")
+        click_mouse()
 
 
-#     return blink_count
+    return blink_count
--- main.py
+++ main.py
@@ -1,117 +1,119 @@
+import webbrowser
+from tkinter import Toplevel
+import tkinter as tk
+import subprocess
+import os
+import time
 from detectors.feature_detector import *
 from detectors.blink_detector import *
 from detectors.keypoint_detector import *
 from mouse_control import *
 import pyautogui
-import time
-<<<<<<< Updated upstream
+import threading
 
-=======
-import tkinter as tk
-
-root = tk.Tk()
-width = root.winfo_screenwidth()
-height = root.winfo_screenheight()
-
->>>>>>> Stashed changes
-pyautogui.FAILSAFE = False
-start_time = time.time()
 capture = cv2.VideoCapture(0)
+screen_x, screen_y = pyautogui.size()
+pyautogui.FAILSAFE = False
+
+
+
+#CALIBRATION PROCESS
+start_time = time.time()
+keypoints = []
+x_min = 10000
+x_max = 0
+y_min = 10000
+y_max = 0
+
+while ((time.time() - start_time) < 15):
+    _, input = capture.read()  # used _ to ignore the boolean returned by .read()
+    input = cv2.flip(input, 1)
+    eye_frames = detect_eyes(input)
+
+    if eye_frames is not None:
+        left_keypoint = find_keypoints(eye_frames[0])
+        right_keypoint = find_keypoints(eye_frames[1])
+        print(left_keypoint, right_keypoint)
+        if left_keypoint and right_keypoint:
+            avg_x = (left_keypoint[0].pt[0] + right_keypoint[0].pt[0]) / 2
+            avg_y = (left_keypoint[0].pt[1] + right_keypoint[0].pt[1]) / 2
+
+            print(avg_x, avg_y)
+            keypoints.append([avg_x, avg_y])
+
+            if len(keypoints) == 2:
+                avg_keypoint = 2 * [0]  # avgs per column
+                nelem = float(2)
+                for col in range(2):
+                    for row in range(2):
+                        avg_keypoint[col] += keypoints[row][col]
+                    avg_keypoint[col] /= nelem
+                print('avg: ', avg_keypoint)
+
+                del keypoints[:]
+
+                x = avg_keypoint[0]
+                y = avg_keypoint[1]
+
+                if x < x_min:
+                    x_min = x
+                elif x > x_max:
+                    x_max = x
+                if y < y_min:
+                    y_min = y
+                elif y > y_max:
+                    y_max = y
+                    
+print(x_min, x_max, y_min, y_max)
+
+
+
+def open_browser():
+    webbrowser.open("https://www.google.ca/?client=safari")
+
+
+def open_file():
+    path = "/Users/chris/Documents/"
+    if os.path.exists(path):
+        subprocess.call(["open", path])
+
+
+window = tk.Tk()
+
+window.geometry("1230x720")
+btn_browser = tk.Button(master=window, width=50,
+                        height=25, text="Web Browser", command=open_browser)
+btn_browser.grid(row=0, column=0, sticky="nsew")
+btn_file = tk.Button(master=window, width=50,
+                     height=25, text="File Browser", command=open_file)
+btn_file.grid(row=0, column=1, sticky="nsew")
+
+
 blink_count = 0
 
 average_x = []
 average_y = []
 count = 0
 
-x_min = 10000
-x_max = 0
-y_min = 10000
-y_max = 0
 
-keypoints = []
-i = 0
-current_position = []
-
-#calibration
-while ((time.time() - start_time) < 15):
-<<<<<<< Updated upstream
-    _, input = capture.read()  # used _ to ignore the boolean returned by .read()
-    input = cv2.flip(input, 1)
-    eye_frames = detect_eyes(input)
-
-    if eye_frames is not None:
-        left_keypoint = find_keypoints(eye_frames[0])
-        right_keypoint = find_keypoints(eye_frames[1])
-        if left_keypoint and right_keypoint:
-            avg_x = (left_keypoint[0].pt[0] + right_keypoint[0].pt[0]) / 2
-            avg_y = (left_keypoint[0].pt[1] + right_keypoint[0].pt[1]) / 2
-
-            keypoints.append([avg_x, avg_y])
-
-            if len(keypoints) == 2:
-                avg_keypoint = 2 * [0]  # avgs per column
-                nelem = float(2)
-                for col in range(2):
-                    for row in range(2):
-                        avg_keypoint[col] += keypoints[row][col]
-                    avg_keypoint[col] /= nelem
-                print('avg: ', avg_keypoint)
-
-                del keypoints[:]
-
-                x = avg_keypoint[0]
-                y = avg_keypoint[1]
-
-                if x < x_min:
-                    x_min = x
-                elif x > x_max:
-                    x_max = x
-                if y < y_min:
-                    y_min = y
-                elif y > y_max:
-                    y_max = y
-
-# center_point = 2*[0] # avgs per column
-# nelem = float(10)
-# for col in range(2):
-#     for row in range(10):
-#         center_point[col] += initial_keypoints[row][col]
-#     center_point[col] /= nelem
-# print('center_point: ', center_point)
-#
-# current_position = center_point
-# pyautogui.moveTo(960, 540)
-# current_mouse_position = [pyautogui.position()[0]][pyautogui.position()[1]]
-print(x_min, x_max, y_min, y_max)
+#MAIN LOOP
 while (1):
+    window.update()
     _, input = capture.read()  # used _ to ignore the boolean returned by .read()
     input = cv2.flip(input, 1)
-
-    # grayscale = cv2.cvtColor(input, cv2.COLOR_BGR2GRAY)
-    # rects = detector(grayscale, 0)
-    # if len(rects) > 0:
-    #     rect = rects[0]
-    # else:
-    #     cv2.imshow("Frame", input)
-    #     k = cv2.waitKey(30) & 0xFF
-    #     continue
-    # blink_count += ear_detector(input,grayscale,rect)
-    # print("blink_count", blink_count)
-=======
-    _, input = capture.read()  # used _ to ignore the boolean returned by .read()
-    input = cv2.flip(input, 1)
->>>>>>> Stashed changes
+    blink_count_new = blink_count
     eye_frames = detect_eyes(input)
 
     if eye_frames is not None:
+        blink_count += ear_detector(input)
         left_keypoint = find_keypoints(eye_frames[0])
         right_keypoint = find_keypoints(eye_frames[1])
+
         if left_keypoint and right_keypoint:
             avg_x = (left_keypoint[0].pt[0] + right_keypoint[0].pt[0]) / 2
             avg_y = (left_keypoint[0].pt[1] + right_keypoint[0].pt[1]) / 2
 
             keypoints.append([avg_x, avg_y])
-<<<<<<< Updated upstream
             if len(keypoints) == 8:
                 avg_keypoint = 2*[0] # avgs per column
                 nelem = float(8)
@@ -119,111 +121,35 @@ while (1):
                     for row in range(8):
                         avg_keypoint[col] += keypoints[row][col]
                     avg_keypoint[col] /= nelem
-                print('avg: ', avg_keypoint)
 
-                #current_keypoint = avg_keypoint
+                print('avg: ', avg_keypoint)
 
                 del keypoints[:] 
 
                 x = avg_keypoint[0]
                 y = avg_keypoint[1]
-                # if count == 20:
-                # move_mouse(average_x, average_y)
 
                 #standardizing x and y coordinates to the size of the screen
-                x_move = (x - x_min) * (2560 / (x_max - x_min))
-                y_move = (y - y_min) * (1440 / (y_max - y_min))
+                x_move = (x - x_min) * (screen_x / (x_max - x_min))
+                y_move = (y - y_min) * (screen_y / (y_max - y_min))
 
                 print('moving to: ', x_move, y)
                 if x_move < 0:
                     x_move = 0
-                elif x_move > 2560:
-                    x_move = 2560
+                elif x_move > screen_x:
+                    x_move = screen_x
                 if y_move < 0:
                     y_move = 0
-                elif y_move > 1440:
-                    y_move = 1440
+                elif y_move > screen_y:
+                    y_move = screen_y
 
                 pyautogui.moveTo(x_move, y_move, 0.5)
-
-    #         else:
-    #             average_x.append(x)
-    #             average_y.append(y)
 
     # print('count: ', count)
-    # if count > 20:
-    #     count = 0
-    # else:
-    #     count += 1
-=======
-
-            if len(keypoints) == 2:
-                avg_keypoint = 2 * [0]  # avgs per column
-                nelem = float(2)
-                for col in range(2):
-                    for row in range(2):
-                        avg_keypoint[col] += keypoints[row][col]
-                    avg_keypoint[col] /= nelem
-                print('avg: ', avg_keypoint)
-
-                del keypoints[:]
-
-                x = avg_keypoint[0]
-                y = avg_keypoint[1]
-
-                if x < x_min:
-                    x_min = x
-                elif x > x_max:
-                    x_max = x
-                if y < y_min:
-                    y_min = y
-                elif y > y_max:
-                    y_max = y
-
-print(x_min, x_max, y_min, y_max)
-while (1):
-    _, input = capture.read()  # used _ to ignore the boolean returned by .read()
-    input = cv2.flip(input, 1)
-    eye_frames = detect_eyes(input)
-
-    if eye_frames is not None:
-        left_keypoint = find_keypoints(eye_frames[0])
-        right_keypoint = find_keypoints(eye_frames[1])
-        if left_keypoint and right_keypoint:
-            avg_x = (left_keypoint[0].pt[0] + right_keypoint[0].pt[0]) / 2
-            avg_y = (left_keypoint[0].pt[1] + right_keypoint[0].pt[1]) / 2
-
-            keypoints.append([avg_x, avg_y])
-            if len(keypoints) == 8:
-                avg_keypoint = 2*[0] # avgs per column
-                nelem = float(8)
-                for col in range(2):
-                    for row in range(8):
-                        avg_keypoint[col] += keypoints[row][col]
-                    avg_keypoint[col] /= nelem
-                print('avg: ', avg_keypoint)
-
-                del keypoints[:] 
-
-                x = avg_keypoint[0]
-                y = avg_keypoint[1]
-
-                #standardizing x and y coordinates to the size of the screen
-                x_move = (x - x_min) * (width / (x_max - x_min))
-                y_move = (y - y_min) * (height / (y_max - y_min))
-
-                print('moving to: ', x_move, y)
-                if x_move < 0:
-                    x_move = 0
-                elif x_move > width:
-                    x_move = width
-                if y_move < 0:
-                    y_move = 0
-                elif y_move > height:
-                    y_move = height
-
-                pyautogui.moveTo(x_move, y_move, 0.5)
->>>>>>> Stashed changes
+    if count > 20:
+        count = 0
+    else:
+        count += 1
 
     k = cv2.waitKey(30) & 0xff
     if k == 27:
